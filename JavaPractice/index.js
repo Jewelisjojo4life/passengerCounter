6fe0a2fbbeed73b287f2002386dfc908ead78dd0
Jojo <jfrasier96@gmail.com>
2023-02-08 09:58:08 -0500

    updates

--- JavaPractice/index.js
+++ JavaPractice/index.js
@@ -1,14 +1,18 @@
-//document.getElementById("count").innerText = 5
+document.getElementById("count").innerText = 5
 
 
 
+let countEl = document.getElementById("count-el")
+console.log("count-el")
+)
 
-
-
-
-
-
+let count = 0
 
 function increment() {
+    
     console.log("The button was clicked")
+count = count + 1
+
 }
+
+increment()
