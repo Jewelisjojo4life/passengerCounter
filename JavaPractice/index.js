ea8625d8f3f971a937c16b60269146a5ec360bf5
Jojo <jfrasier96@gmail.com>
2023-02-09 01:11:33 -0500

    incrment functions now working

--- JavaPractice/index.js
+++ JavaPractice/index.js
@@ -1,18 +1,15 @@
-document.getElementById("count").innerText = 5
-
-
+//document.getElementById("count").innerText = 5
 
 let countEl = document.getElementById("count-el")
-console.log("count-el")
-)
 
 let count = 0
 
 function increment() {
-    
-    console.log("The button was clicked")
 count = count + 1
+console.log =(count)
+
+countEl.innerText = count
 
 }
 
-increment()
+console.log("The button was clicked")
