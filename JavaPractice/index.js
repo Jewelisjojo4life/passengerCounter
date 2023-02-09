d42c35ff79d3c20c94f1ecb7368cb1031cb68824
Jojo <jfrasier96@gmail.com>
2023-02-09 03:15:43 -0500

    counter completed

--- JavaPractice/index.js
+++ JavaPractice/index.js
@@ -1,15 +1,34 @@
-//document.getElementById("count").innerText = 5
+//document.getElementById("count").textContent = 5
 
 let countEl = document.getElementById("count-el")
 
+let saveEl = document.getElementById("save-el")
 let count = 0
 
 function increment() {
 count = count + 1
-console.log =(count)
-
-countEl.innerText = count
+countEl.textContent = count
 
 }
 
-console.log("The button was clicked")
+
+function save()  {
+    let dash = count + " - "
+   saveEl.textContent += dash
+   countEl.textContent =0
+   count = 0
+   console.log(count)
+}
+
+
+//let welcomeEl = document.getElementById("welcome-el")
+
+
+
+
+
+
+
+
+
+
